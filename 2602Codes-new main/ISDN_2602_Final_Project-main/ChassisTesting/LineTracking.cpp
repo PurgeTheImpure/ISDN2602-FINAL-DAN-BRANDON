a34392883ce51745d23a07536a415e8e9f7f4f33
PurgeTheImpure <145426869+PurgeTheImpure@users.noreply.github.com>
2024-05-16 02:02:21 +0800

    new

--- 2602Codes-new main/ISDN_2602_Final_Project-main/ChassisTesting/LineTracking.cpp
+++ 2602Codes-new main/ISDN_2602_Final_Project-main/ChassisTesting/LineTracking.cpp
@@ -1,5 +1,8 @@
 #include "LineTracking.hpp"
 
+#define turnTime90 700
+#define turnTime45 400
+
 void LineTracking::FollowingLine( uint8_t Case, uint16_t LeftSpeed, uint16_t RightSpeed, int Task, int &CornerNumApproach){
   //Serial.println(getTagUID());
 
@@ -9,46 +12,123 @@ void LineTracking::FollowingLine( uint8_t Case, uint16_t LeftSpeed, uint16_t Rig
     Motor::Stop();
     delay(30);
   }
-  
-  switch(CornerNumApproach) {
 
-    case 100:
-      Motion::Boost(350, 365);
-      delay(100);
-      Motion::Boost(650, 666);
-      delay(800);
-      Motor::Stop();
-      delay(500);
-      CornerNumApproach = 0;
-      break;
-    
-    case 2: 
-      Motion::Rightwards(LeftSpeed, RightSpeed);
-      delay(700);
-      CornerNumApproach = 0;
-      break;
-    
-    case 3: 
-      Motion::Forwards(LeftSpeed, RightSpeed);
-      delay(500);
-      CornerNumApproach = 0;
-      break;
-    
-    case 4: 
-      Motion::Forwards(LeftSpeed, RightSpeed);
-      delay(500);
-      CornerNumApproach = 0;
-      break;
-    
-    case 5:
-      Motor::Stop();
-      delay(3000);
-      CornerNumApproach = 0;
-      break;
+  if (Task == 1 || ) {
+    switch(CornerNumApproach) {
+      
+      case 2: 
+        Motion::Rightwards(LeftSpeed, RightSpeed);
+        delay(turnTime45);
+        CornerNumApproach = 0;
+        break;
+      
+      case 3: 
+        Motion::Forwards(LeftSpeed, RightSpeed);
+        delay(500);
+        CornerNumApproach = 0;
+        break;
+      
+      case 4: 
+        Motion::Forwards(LeftSpeed, RightSpeed);
+        delay(500);
+        CornerNumApproach = 0;
+        break;
+      
+      case 5:
+        Motor::Stop();
+        delay(3000);
+        CornerNumApproach = 0;
+        break;
 
-    default:
-      break;
+      case 100:
+        Motion::Boost(350, 365);
+        delay(100);
+        Motion::Boost(650, 666);
+        delay(800);
+        Motor::Stop();
+        delay(500);
+        CornerNumApproach = 0;
+        break;
 
+      default:
+        break;
+
+    }
+  }
+
+  else if (Task == 3) {
+    if (startpt == 11 || startpt == 12) {
+      switch (CornerNumApproach) {
+        case 11:
+          Motion::Rightwards(LeftSpeed, RightSpeed);
+          delay(turnTime90);
+          CornerNumApproach = 0;
+          break;
+        
+        case 6:
+          Motion::Forwards(LeftSpeed, RightSpeed);
+          delay(turnTime90);
+          CornerNumApproach = 0;
+          break;
+
+        case 2:
+          Motion::Forwards(LeftSpeed, RightSpeed);
+          delay(turnTime90);
+          CornerNumApproach = 0;
+          break;
+
+        case 1:
+          Motor::Stop();
+          delay(3000);
+          CornerNumApproach = 0;
+          break;
+
+        case 100:
+          Motion::Boost(350, 365);
+          delay(100);
+          Motion::Boost(650, 666);
+          delay(800);
+          Motor::Stop();
+          delay(500);
+          CornerNumApproach = 0;
+          break;
+
+      }
+    }
+
+    else if (startpt == 13 || startpt == 14) {
+
+      switch (CornerNumApproach) {
+        case 13:
+          Motion::Forwards(LeftSpeed, RightSpeed);
+          delay(turnTime90);
+          CornerNumApproach = 0;
+          break;
+        
+        case 5:
+          Motion::Forwards(LeftSpeed, RightSpeed);
+          delay(turnTime90);
+          CornerNumApproach = 0;
+          break;
+
+        case 1:
+          Motor::Stop();
+          delay(3000);
+          CornerNumApproach = 0;
+          break;
+
+        case 100:
+          Motion::Boost(350, 365);
+          delay(100);
+          Motion::Boost(650, 666);
+          delay(800);
+          Motor::Stop();
+          delay(500);
+          CornerNumApproach = 0;
+          break;
+
+      }
+    }
   }
 
   switch (Case) {
@@ -99,15 +179,6 @@ void LineTracking::FollowingLine( uint8_t Case, uint16_t LeftSpeed, uint16_t Rig
     break;
   }
   
-
-  //String x_fuckyou = 20D96691;
-  /*
-  if (tempIdTag == "20d96691" || tempIdTag == "f0de6691" || tempIdTag == "a0db6691") {
-    Motor::Stop();
-    delay(4500);
-  }
-  */
-
 
 
 };
--- 2602Codes-new main/ISDN_2602_Final_Project-main/ChassisTesting/LineTracking.hpp
+++ 2602Codes-new main/ISDN_2602_Final_Project-main/ChassisTesting/LineTracking.hpp
@@ -8,7 +8,10 @@
 #define NearBend 1
 #define NearCorner 2
 
-
+extern int path[14];
+extern int pathLength = 0;
+extern int startpt = 11; // Example start node
+extern int endpt = 5;   // Example end node
 
 namespace LineTracking {
   void FollowingLine( uint8_t Case, uint16_t LeftSpeed, uint16_t RightSpeed, int Task, int &CornerNumApproach);
